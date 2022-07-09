331516e23e82bd27de36fe04135a86f62f4bd484
The Scam. Scam <108811696+thescammers@users.noreply.github.com>
2022-07-09 13:20:58 +0000

    d

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import React, {useState, useEffect} from 'react';
 import Contents from './Contents'
